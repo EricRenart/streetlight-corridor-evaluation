376942f2766ed82558e4b35b205ad8f0cbb70922
MuchaHawlucha <Zinric1216>
2020-04-07 10:27:30 -0400

    split tables into AM and PM to sum up DMFs

--- src/Exposure.py
+++ src/Exposure.py
@@ -1,9 +1,24 @@
 import pandas as pd
 import openpyxl as opxl
 import os
+from tabulate import tabulate
 from tkinter.filedialog import askopenfilename, askdirectory, asksaveasfile
 
 # ---- This script calculates exposure from Streetlight data. ----
+HEADER_MF = "Middle Filter Zone Name"
+HEADER_DEST = "Destination Zone Name"
+HEADER_OMD = "Average Daily O-M-D Traffic (StL Index)"
+HEADER_DAY_PART = "Day Part"
+HEADER_DAY_TYPE = "Day Type"
+
+# pretty-prints a dataframe with StreetLight Data with the help of tabulate
+def prettyprint(stl_dataframe):
+    tabulate_header = ['ID', 'DESTINATION', 'MIDDLE FILTER', 'DAY PART', 'OMD TRAFFIC']
+    print(tabulate(stl_dataframe, headers=tabulate_header, tablefmt='psql'))
+
+def isDMF(string):
+    if string.__contains__("DMF"): return True
+    else: return False
 
 # for each "before" project...
 # open a file chooser to pick the before csv
@@ -12,20 +27,34 @@ print('CSV selected: '+before_path)
 
 # open a file chooser again to pick the corresponding "after" csv
 after_path = askopenfilename(title='Choose after csv:')
+print('CSV selected: '+after_path)
 
 # open csv's
 before_dataframe = pd.read_csv(before_path)
 after_dataframe = pd.read_csv(after_path)
 
 # filter out "All Days: and "All Day" day types/day parts
-before_dataframe_weekday = before_dataframe[before_dataframe["Day Type"] == "1: Weekday (Tu-Th)"]
+before_dataframe_weekday = before_dataframe[before_dataframe[HEADER_DAY_TYPE] == "1: Weekday (Tu-Th)"]
 
 # get the weekday AM and PM OMD traffic
-OMD_traffic_weekday = before_dataframe_weekday[["Destination Zone Name","Middle Filter Zone Name","Day Part","Average Daily O-M-D Traffic (StL Index)"]]
-OMD_traffic_AM = OMD_traffic_weekday[OMD_traffic_weekday["Day Part"] == "1: Peak AM (7am-10am)"]
-OMD_traffic_PM = OMD_traffic_weekday[OMD_traffic_weekday["Day Part"] == "2: Peak PM (4pm-7pm)"]
-print(OMD_traffic_AM)
-print(OMD_traffic_PM)
+OMD_traffic_weekday = before_dataframe_weekday[[HEADER_DEST,HEADER_MF,HEADER_DAY_PART,HEADER_DAY_TYPE]]
+OMD_traffic_AM = OMD_traffic_weekday[OMD_traffic_weekday[HEADER_DAY_PART] == "1: Peak AM (7am-10am)"]
+OMD_traffic_PM = OMD_traffic_weekday[OMD_traffic_weekday[HEADER_DAY_PART] == "2: Peak PM (4pm-7pm)"]
+print('AM OMD TRAFFIC')
+prettyprint(OMD_traffic_AM)
+print('PM OMD TRAFFIC')
+prettyprint(OMD_traffic_PM)
+
+# sum the DMF traffic for each destination
+# get all rows which contain DMF
+am_dmfs = OMD_traffic_AM[OMD_traffic_AM[HEADER_MF].str.contains("DMF")]
+pm_dmfs = OMD_traffic_PM[OMD_traffic_PM[HEADER_MF].str.contains("DMF")]
+
+print(am_dmfs)
+print(pm_dmfs)
+
+# for each destination add the corresponding dmf's
+
 
 # calculate exposure: ExpIndex = MF_i/DMF_i
 # do the same for the "After" condition
