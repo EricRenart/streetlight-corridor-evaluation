f6e35ad5e263386f5e4d9f9a2f03c876b438945d
MuchaHawlucha <Zinric1216>
2020-04-07 16:41:04 -0400

    fixed bug with DMF traffic column, however, DMFs must be named [destination name]+DMF

--- src/Exposure.py
+++ src/Exposure.py
@@ -26,6 +26,19 @@ def sumDMF(df):
     print(tabulate(sumdmf))
     return sumdmf
 
+def fillDMFTraffic(merged_df, dmf_df):
+    for row in range(0,len(merged_df)):
+        cur_destination = merged_df.iloc[row,0]
+        cur_dmf = cur_destination + " DMF"
+        print(dmf_df)
+        if merged_df.iloc[row,1].__eq__(cur_dmf):
+            cur_dmf_traffic = dmf_df.loc[cur_dmf,HEADER_OMD]
+            for row1 in range(0,len(merged_df)):
+                if merged_df.iloc[row1,0].__eq__(cur_destination):
+                    merged_df.iloc[row1,5] = cur_dmf_traffic
+            #merged_df.iloc[row,5] = cur_dmf_traffic
+    return merged_df
+
 # --- for each "before" project... ---
 # ---open a file chooser to pick the before csv ---
 before_path = askopenfilename(title='Choose before csv:')
@@ -54,17 +67,26 @@ print(tabulate(OMD_traffic_PM))
 # --- sum the DMF traffic for each destination ---
 
 # get all rows which contain DMF and sum
-
 print('TOTAL AM DMF TRAFFIC')
 am_dmf_sum = sumDMF(OMD_traffic_AM)
 print('TOTAL PM DMF TRAFFIC')
 pm_dmf_sum = sumDMF(OMD_traffic_PM)
 
 # --- calculate exposure: ExpIndex = MF_i/DMF_i ---
+# associate each DMF with its destination
 
-# do the same for the "After" condition
+merged_dmf_am = OMD_traffic_AM.merge(am_dmf_sum,on=HEADER_MF,how="outer")
+merged_dmf_pm = OMD_traffic_PM.merge(pm_dmf_sum,on=HEADER_MF,how="outer")
+print(tabulate(merged_dmf_pm))
+
+# now assign sumDMF to all rows based on destination
+filled_dmf_am = fillDMFTraffic(merged_dmf_am, am_dmf_sum)
+filled_dmf_pm = fillDMFTraffic(merged_dmf_pm, pm_dmf_sum)
+
+print(tabulate(filled_dmf_am))
+print(tabulate(filled_dmf_pm))
 
 # for each MF in each destination in each project, subtract each "After" exposure index from each "Before" exposure index to get the change in exposure
-# average the exposure differences, ignoring rows with missing exposures because only a before or after exposure value without its "twin" can't be compared
+# average the exposure differences, ignoring rows with missing exposures because only a before or after exposure value without its "twin" can't be compared (inner join?)
 
 # write results to an excel file
