d2d2ce3e89f275ac11b7b5f7d6c5d702bd141b57
MuchaHawlucha <Zinric1216>
2020-04-08 10:23:06 -0400

    finished implementing calculations and implemented writing out a results file

--- src/Exposure.py
+++ src/Exposure.py
@@ -23,7 +23,6 @@ def getDMFs(df):
 def sumDMF(df):
     dmfs = getDMFs(df)
     sumdmf = dmfs.groupby(HEADER_MF).sum()
-    print(tabulate(sumdmf))
     return sumdmf
 
 def fillDMFTraffic(merged_df, dmf_df):
@@ -54,39 +53,62 @@ after_dataframe = pd.read_csv(after_path)
 
 # --- filter out "All Days: and "All Day" day types/day parts ---
 before_dataframe_weekday = before_dataframe[before_dataframe[HEADER_DAY_TYPE] == "1: Weekday (Tu-Th)"]
+after_dataframe_weekday = after_dataframe[after_dataframe[HEADER_DAY_TYPE] == "1: Weekday (Tu-Th)"]
 
 # --- get the weekday AM and PM OMD traffic ---
 OMD_traffic_weekday = before_dataframe_weekday[[HEADER_DEST,HEADER_MF,HEADER_DAY_PART,HEADER_DAY_TYPE,HEADER_OMD]]
+OMD_traffic_weekday_after = after_dataframe_weekday[[HEADER_DEST,HEADER_MF,HEADER_DAY_PART,HEADER_DAY_TYPE,HEADER_OMD]]
 OMD_traffic_AM = OMD_traffic_weekday[OMD_traffic_weekday[HEADER_DAY_PART] == "1: Peak AM (7am-10am)"]
 OMD_traffic_PM = OMD_traffic_weekday[OMD_traffic_weekday[HEADER_DAY_PART] == "2: Peak PM (4pm-7pm)"]
-print('AM OMD TRAFFIC')
-print(tabulate(OMD_traffic_AM))
-print('PM OMD TRAFFIC')
-print(tabulate(OMD_traffic_PM))
-
+OMD_traffic_AM_after = OMD_traffic_weekday_after[OMD_traffic_weekday_after[HEADER_DAY_PART] == "1: Peak AM (7am-10am)"]
+OMD_traffic_PM_after = OMD_traffic_weekday_after[OMD_traffic_weekday_after[HEADER_DAY_PART] == "2: Peak PM (4pm-7pm)"]
 # --- sum the DMF traffic for each destination ---
 
 # get all rows which contain DMF and sum
-print('TOTAL AM DMF TRAFFIC')
+print('TOTAL AM DMF TRAFFIC - BEFORE')
 am_dmf_sum = sumDMF(OMD_traffic_AM)
-print('TOTAL PM DMF TRAFFIC')
+print('TOTAL PM DMF TRAFFIC - BEFORE')
 pm_dmf_sum = sumDMF(OMD_traffic_PM)
+print('TOTAL AM DMF TRAFFIC - AFTER')
+am_dmf_sum_after = sumDMF(OMD_traffic_AM_after)
+print('TOTAL PM DMF TRAFFIC - AFTER')
+pm_dmf_sum_after = sumDMF(OMD_traffic_PM_after)
 
 # --- calculate exposure: ExpIndex = MF_i/DMF_i ---
 # associate each DMF with its destination
 
 merged_dmf_am = OMD_traffic_AM.merge(am_dmf_sum,on=HEADER_MF,how="outer")
 merged_dmf_pm = OMD_traffic_PM.merge(pm_dmf_sum,on=HEADER_MF,how="outer")
-print(tabulate(merged_dmf_pm))
+merged_dmf_am_after = OMD_traffic_AM_after.merge(am_dmf_sum_after,on=HEADER_MF,how="outer")
+merged_dmf_pm_after = OMD_traffic_PM_after.merge(pm_dmf_sum_after,on=HEADER_MF,how="outer")
 
 # now assign sumDMF to all rows based on destination
 filled_dmf_am = fillDMFTraffic(merged_dmf_am, am_dmf_sum)
 filled_dmf_pm = fillDMFTraffic(merged_dmf_pm, pm_dmf_sum)
+filled_dmf_am_after = fillDMFTraffic(merged_dmf_am_after, am_dmf_sum_after)
+filled_dmf_pm_after = fillDMFTraffic(merged_dmf_pm_after, pm_dmf_sum_after)
 
-print(tabulate(filled_dmf_am))
-print(tabulate(filled_dmf_pm))
+# add a new column and calculate exposure index
+filled_dmf_am["Exposure Index (AM Before)"] = (filled_dmf_am["Average Daily O-M-D Traffic (StL Index)_x"] / filled_dmf_am["Average Daily O-M-D Traffic (StL Index)_y"]) * 100
+filled_dmf_pm["Exposure Index (PM Before)"] = (filled_dmf_pm["Average Daily O-M-D Traffic (StL Index)_x"] / filled_dmf_pm["Average Daily O-M-D Traffic (StL Index)_y"]) * 100
+filled_dmf_am_after["Exposure Index (AM After)"] = (filled_dmf_am_after["Average Daily O-M-D Traffic (StL Index)_x"] / filled_dmf_am_after["Average Daily O-M-D Traffic (StL Index)_y"]) * 100
+filled_dmf_pm_after["Exposure Index (PM After)"] = (filled_dmf_pm_after["Average Daily O-M-D Traffic (StL Index)_x"] / filled_dmf_pm_after["Average Daily O-M-D Traffic (StL Index)_y"]) * 100
+
 
 # for each MF in each destination in each project, subtract each "After" exposure index from each "Before" exposure index to get the change in exposure
 # average the exposure differences, ignoring rows with missing exposures because only a before or after exposure value without its "twin" can't be compared (inner join?)
+exposure_am = pd.merge(filled_dmf_am,filled_dmf_am_after,how="inner",on=[HEADER_DEST,HEADER_MF,HEADER_DAY_PART])
+exposure_pm = pd.merge(filled_dmf_pm,filled_dmf_pm_after,how="inner",on=[HEADER_DEST,HEADER_MF,HEADER_DAY_PART])
 
-# write results to an excel file
+exposure_am["% Exposure Change AM"] = exposure_am["Exposure Index (AM After)"] - exposure_am["Exposure Index (AM Before)"]
+exposure_pm["% Exposure Change PM"] = exposure_pm["Exposure Index (PM After)"] - exposure_pm["Exposure Index (PM Before)"]
+
+# Create a summary table with the changes in exposure
+summary_am = pd.DataFrame([exposure_am[HEADER_DEST],exposure_am[HEADER_MF],exposure_am["Exposure Index (AM Before)"],exposure_am["Exposure Index (AM After)"],exposure_am["% Exposure Change AM"]]).transpose()
+summary_am_sorted = summary_am.sort_values(HEADER_DEST)
+summary_pm = pd.DataFrame([exposure_pm[HEADER_DEST],exposure_pm[HEADER_MF],exposure_pm["Exposure Index (PM Before)"],exposure_pm["Exposure Index (PM After)"],exposure_pm["% Exposure Change PM"]]).transpose()
+summary_pm_sorted = summary_pm.sort_values(HEADER_DEST)
+
+# write out the results as a csv
+summary_am_sorted.to_csv("exposure_am.csv")
+summary_pm_sorted.to_csv("exposure_pm.csv")
